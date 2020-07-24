6a1e9a02b9184f38c2efe1d6cac85c98d1c774c5
tg2648 <45052145+tg2648@users.noreply.github.com>
2020-07-24 10:27:13 -0400

    app factory

--- app/__init__.py
+++ app/__init__.py
@@ -55,6 +55,11 @@ def create_app():
     # from app.deptprofile.callbacks import register_deptprofile_callbacks as deptprofile_callbacks
     # register_dashapp(server, 'Dept. Profile', 'deptprofile', deptprofile_layout, deptprofile_callbacks)
 
+    # Register document repository
+    from app.repository.layout import serve_repository_layout as repository_layout
+    from app.repository.callbacks import register_repository_callbacks as repository_callbacks
+    register_dashapp(server, 'Faculty Governance', 'faculty_governance', repository_layout, repository_callbacks)
+
     return server
 
 
@@ -97,7 +102,7 @@ def register_dashapp(app, title, base_pathname, serve_layout, register_callbacks
             __name__,
             server=app,
             url_base_pathname=f'/{base_pathname}/',
-            assets_folder=f'{get_root_path(__name__)}/assets/',
+            assets_folder=f'{get_root_path(__name__)}/dash_assets/',
             assets_external_path='https://cu-dash-static.s3.us-east-2.amazonaws.com/assets/',
             external_stylesheets=[dbc.themes.CERULEAN]
         )
@@ -140,7 +145,7 @@ def register_dashapp(app, title, base_pathname, serve_layout, register_callbacks
         register_callbacks(my_dashapp)
 
     # Require login to access Dash pathnames
-    _protect_dashviews(my_dashapp)
+    # _protect_dashviews(my_dashapp)
 
 
 def _protect_dashviews(dashapp):
@@ -192,8 +197,14 @@ def register_blueprints(server):
     from app.views import fif_archive
     from app.views import fif_changelog
     from app.views import lab_occupancy
+    from app.views import repository
 
     server.register_blueprint(home.bp)
-    server.register_blueprint(lab_occupancy.bp)
     server.register_blueprint(fif_archive.bp)
     server.register_blueprint(fif_changelog.bp)
+    server.register_blueprint(lab_occupancy.bp)
+    server.register_blueprint(repository.bp)
+
+    # Register errors
+    from app.errors import handlers
+    server.register_blueprint(handlers.bp)
