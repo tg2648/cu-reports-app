58aec7b822cadeb50902afcb4f47f45162d2731d
tg2648 <45052145+tg2648@users.noreply.github.com>
2020-12-03 12:57:59 -0500

    [facgov] add contact us button

--- app/facgov/layouts/header.py
+++ app/facgov/layouts/header.py
@@ -4,21 +4,58 @@ Layout: Top row
 
 # Third party imports
 import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 
 
-title = html.Div(
+contact_text = dcc.Markdown(
+    '''
+    For any questions, please contact Timur Gulyamov (<tg2648@columbia.edu>) or Rose Razaghian (<rr222@columbia.edu>).
+    '''
+)
+
+contact_button = dbc.Button(
+    "Contact us",
+    id="contact-popup-button",
+    color="link",
+    outline=False
+)
+
+contact_popup = dbc.Modal(
+    [
+        dbc.ModalHeader(html.Span('Contact us', className='text-info')),
+        dbc.ModalBody(contact_text),
+        dbc.ModalFooter(
+            dbc.Button("Close", id="close-contact", className="ml-auto", size='sm')
+        ),
+    ],
+    id="contact-popup"
+)
+
+button_group = dbc.Col(
+    dbc.ButtonGroup(
+        [
+            contact_button,
+        ],
+        size='sm'
+    ),
+    width='auto'
+)
+
+title = dbc.Col(
     html.H5('A&S Faculty Governance', className='text-info mb-0'),
-    className='col'
 )
 
 header = html.Div(
     [
-        html.Div(
+        dbc.Row(
             [
                 title,
+                button_group,
+                contact_popup,
             ],
-            className='row mt-4'
+            className='mt-4'
         ),
-        html.Hr()
+        html.Hr(className='mt-0'),
     ]
 )
